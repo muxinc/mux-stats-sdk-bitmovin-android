90c948638c0821ae806c0820d1d4f837b863296c
Tomislav Kordic <tkordic.private@gmail.com>
2021-12-21 11:05:48 +0100

    app

---
+++ app/src/no_ads/java/com/mux/stats/sdk/muxstats/bitmovinplayer/demo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.mux.stats.sdk.muxstats.bitmovinplayer.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+
+import com.bitmovin.player.BitmovinPlayer;
+import com.bitmovin.player.BitmovinPlayerView;
+import com.bitmovin.player.config.PlayerConfiguration;
+import com.bitmovin.player.config.media.SourceConfiguration;
+import com.mux.stats.sdk.core.model.CustomerPlayerData;
+import com.mux.stats.sdk.core.model.CustomerVideoData;
+import com.mux.stats.sdk.muxstats.bitmovinplayer.MuxStatsSDKBitmovinPlayer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BitmovinPlayerView bitmovinPlayerView;
+    private BitmovinPlayer bitmovinPlayer;
+    private MuxStatsSDKBitmovinPlayer muxStats;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.bitmovinPlayerView = (BitmovinPlayerView) this.findViewById(R.id.bitmovinPlayerView);
+        this.bitmovinPlayer = this.bitmovinPlayerView.getPlayer();
+
+        this.initializePlayer();
+        configureMuxSdk();
+    }
+
+    private void configureMuxSdk() {
+        CustomerPlayerData customerPlayerData = new CustomerPlayerData();
+        customerPlayerData.setEnvironmentKey("YOUR ENVIRONMENT KEY HERE");
+        CustomerVideoData customerVideoData = new CustomerVideoData();
+        customerVideoData.setVideoTitle("Sintel");
+        muxStats = new MuxStatsSDKBitmovinPlayer(
+                this,
+                bitmovinPlayerView,
+                "demo-view-player",
+                customerPlayerData,
+                customerVideoData);
+
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        muxStats.setScreenSize(size.x, size.y);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        this.bitmovinPlayerView.onStart();
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        this.bitmovinPlayerView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        this.bitmovinPlayerView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        this.bitmovinPlayerView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        this.bitmovinPlayerView.onDestroy();
+        super.onDestroy();
+    }
+
+    protected void initializePlayer()
+    {
+        // Create a new source configuration
+        SourceConfiguration sourceConfiguration = new SourceConfiguration();
+
+        // Add a new source item
+        sourceConfiguration.addSourceItem("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd");
+        PlayerConfiguration playerConfiguration = new PlayerConfiguration();
+        playerConfiguration.setSourceConfiguration(sourceConfiguration);
+        // load source using the created source configuration
+        this.bitmovinPlayer.setup(playerConfiguration);
+//        bitmovinPlayer.play();
+    }
+}
